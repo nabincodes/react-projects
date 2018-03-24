574affdf117ba066b85ff18d1f20e474485306bc
Nabin Jaiswal <nabinjaiswal@gmail.com>
2018-03-24 10:37:09 +0530

    Minor styling changes in Layout

--- burger-web-app/src/components/Layout/Layout.js
+++ burger-web-app/src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import classes from './Layout.css';
 import Aux from '../../hoc/Auxi';
 
 const layout = (props) =>(
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop </div>
 
-        <main>
+        <main className={classes.content}>
             {props.children}
         </main>
 
